2ece8f5c722f80a11ea35de7dc38fe8966c43535
Lydie Umuhire <umuhirelydie2@gmail.com>
2023-06-26 21:37:17 -0700

    fix betty coding style

--- print-d.c
+++ print-d.c
@@ -12,7 +12,7 @@ int print_integer(va_list args)
 	int nb = va_arg(args, int);
 	int nbr, lst = nb % 10, dgt, expon = 1, j = 1;
 
-	nb = n / 10;
+	nb = nb / 10;
 	nbr = nb;
 
 	if (lst < 0)
@@ -25,22 +25,18 @@ int print_integer(va_list args)
 	}
 	if (nbr > 0)
 	{
-		do
-		{
+		do {
 			expon = expon * 10;
 			nbr = nbr / 10;
-		}
-		while (nbr / 10 != 0);
+		} while (nbr / 10 != 0);
 		nbr = nb;
-		do
-		{
+		do {
 			dgt = nbr / expon;
 			_putchar(dgt + '0');
 			nbr = nbr - (dgt * expon);
 			expon = expon / 10;
 			j++;
-		}
-		while(expon > 0);
+		} while (expon > 0);
 	}
 	_putchar(lst + '0');
 
